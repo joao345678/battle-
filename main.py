61075ad994eab038449144d109d1c34ccfaaa406
joao345678 <153541296+joao345678@users.noreply.github.com>
2024-08-31 21:09:23 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from character import Hero,Enemy
+from weapon import short_bow, iron_sword
+
+hero = Hero(name = 'Hero' , health = 100)
+hero.equip(iron_sword)
+enemy = Enemy(name = "Enemy", health = 100, weapon = short_bow)
+
+
+while True:
+    os.system('cls')
+    
+    hero.attack(enemy)
+    enemy.attack(hero)
+    
+    input()
